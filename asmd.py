d155918fb61f43064e6fd248fda692068373ddce
prateeky477 <96304501+prateeky477@users.noreply.github.com>
2021-12-30 22:44:43 +0530

    Update asmd.py

--- asmd.py
+++ asmd.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 
 st.title("Welcome to this page")
-st.header("This page will help you to perform Addition Subtraction Multipllication and Division on two number")
+st.header("This page will help you to perform Addition Subtraction Multiplication and Division on two number")
 c=st.selectbox("Choose what you want to perform",["Addition","Subtraction","Multiplication","Division"])
 a=st.number_input("Enter first number")
 b=st.number_input("Enter second number")
